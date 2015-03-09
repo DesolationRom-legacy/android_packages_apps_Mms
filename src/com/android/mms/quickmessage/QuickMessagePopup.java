69ffc27dff4242fd042a1d3b4f7a4368d2b0d178
liquid0624 <rjbirnesser@gmail.com>
2015-03-09 17:50:45 -0400

    Mms: missing quick message popup import

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -20,6 +20,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.KeyguardManager;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
